2e3a35a1feafc52e4b2642b7f0c0f2b4fcce28a6
Mike <amaglichc@gmail.com>
2023-09-18 12:37:39 +0300

    first version calculator on cpp

---
+++ test.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+using namespace std; 
+int main() {
+	setlocale(LC_ALL, "RU");
+	int a, b;
+	char sym;
+	cout << "Введите первое число:";
+	cin >> a;
+	cout << "Введите второе число:";
+	cin >> b;
+	int result;
+	cout << "Введите оператор:";
+	cin >> sym;
+	if (sym == '+') {
+		result = a + b;
+		cout << a << "+" << b << "=" << result;
+		exit(0);
+	}
+	if (sym == '-') {
+		result = a - b;
+		cout << a << "-" << b << "=" << result;
+		exit(0);
+	}
+	if (sym == '*') {
+		result = a * b;
+		cout << a << "*" << b << "=" << result;
+		exit(0);
+
+	}
+	if (sym == '/') {
+		result = a / b;
+		cout << a << "/" << b << "=" << result;
+		exit(0);
+
+	}
+	else {
+		cout << "oops ERROR";
+		exit(0);
+
+	}
+	return 0;
+} 
